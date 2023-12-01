10116c3b23e598f3fa9d43119612e61193ad9f9a
William Suarez <147040637+Suarwill@users.noreply.github.com>
2023-12-01 20:52:19 -0300

    Actualizacion script.js
    creando traductor

--- Prueba03/script.js
+++ Prueba03/script.js
@@ -4,26 +4,47 @@ window.onload = () =>{
 const showOpenai = () =>{
     app = document.getElementById('app')
     app.innerHTML = ''
-    
         label_prompt = document.createElement('label')
         label_prompt.classList.add('label')
         label_prompt.for = 'prompt'
 
-    // Creando área para ingresar la formula a calcular
+    // Creando área para ingresar la frase a traducir
     const sol_prompt = document.createElement('textarea')
-        sol_prompt.placeholder = 'Ingrese la formula que desea calcular:'
+        sol_prompt.placeholder = 'Ingrese la frase a traducir:'
         sol_prompt.id = 'prompt'
         sol_prompt.classList.add('form-control')
-        sol_prompt.style.height= '60px'
+        sol_prompt.style.height= '300px'
         sol_prompt.style.width= '500px'
         sol_prompt.style.backgroundImage = 'linear-gradient(180deg, #adffff 0, #a4ffff 25%, #9df2f2 50%, #97dade 75%, #92c5cb 100%)'
         app.appendChild(sol_prompt)
     var br = document.createElement('br')
     app.append(br)
 
+    //Creando el Opcional de Idioma a traducir
+        label_idioma = document.createElement('label')
+        label_idioma.innerText = 'Traducir al siguiente Idioma :'
+        label_idioma.classList.add('label')
+        label_idioma.for = 'idioma'
+        app.appendChild(label_idioma)
+    const sl_idioma = document.createElement('select')
+        sl_idioma.id = 'idioma'
+        sl_idioma.classList.add('form-control')
+        sl_idioma.style.width= '200px'
+        idiomas = ["Inglés",    "Chino Mandarín",   "Portugués",
+                    "Italiano", "Bengalí",          "Frances",
+                    "Ruso",     "Japonés",          "Turco",
+                    "Árabe",    "Hindú",            "Danés"]
+        idiomas.forEach(idioma => {
+        opt = document.createElement('option')
+        opt.innerText= idioma
+        sl_idioma.appendChild(opt)
+        });
+        app.appendChild(sl_idioma)
+        app.append(br)
+
     // Creando Botones, se agrego un fondo de color degradado
     const button = document.createElement('button')
-        button.innerText = "Calcular"
+        button.innerText = "Traducir"
         button.classList.add('btn','btn-success')
         button.style.color= 'green'
         button.style.backgroundImage = 'linear-gradient(145deg, #d1fff8 0, #befff6 10%, #a7fff4 20%, #8dfff3 30%, #6cfbf2 40%, #3cf2f2 50%, #00e8f3 60%, #00dff5 70%, #00d6f9 80%, #00cefd 90%, #00c6ff 100%)'
@@ -53,13 +74,13 @@ const enviarOpenAi = async () =>{
 
     // configurando openAI (URL y APIKEY)    
     var url = "https://api.openai.com/v1/chat/completions";
-    var api = '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'
+    var api = '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'
     var bearer = `Bearer ${api}`
 
     // Estructura de la solicitud A OPENAI
     const prompt = document.getElementById('prompt').value
-    const respuestaNegativa = 'Favor ingresa una fórmula matemática'
-    const solicitud = `Calcula lo siguiente " ${prompt} " y muestra solo el resultado, si lo anterior no es posible calcular, solo responde ${respuestaNegativa} `
+    const idiomaTraducir = document.getElementById('idioma').value
+    const solicitud = `" ${prompt} " traduce lo anterior dicho al siguiente idioma: "${idiomaTraducir}" `
     const requestOptions = {
         method: 'POST',
         headers: {
@@ -91,7 +112,7 @@ const enviarOpenAi = async () =>{
         divBody = document.createElement('div')
         h5 = document.createElement('h6')
             h5.classList.add('card-title')
-            h5.innerText = "La respuesta es la siguiente:"
+            h5.innerText = "La traducción es la siguiente:"
             h5.style.padding = "20px"
         parrafo = document.createElement('p')
             parrafo.classList.add("card-text")
